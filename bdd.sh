e82b319c3745eb5bf0b8e0884e76d47f9019fe39
luctst <lucas.tostee@gmail.com>
2021-05-09 12:59:46 +0200

    Update environment setup

--- bdd.sh
+++ bdd.sh
@@ -2,6 +2,7 @@
 
 getBackup() {
     echo "Retrieving last backup."
+    docker exec --env-file='./.env' -i db bash -c 'mongodump --archive="./tmp/dump-bizzy.gz" --gzip --db bizzy-dev --uri "$DB_URL" && mongorestore --gzip --nsInclude="bizzy-dev.*" --archive="./tmp/dump-bizzy-dev.gz"'
 }
 
 getBackup
